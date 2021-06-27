371c72b8ed9659f1aad79ba08a3a7b926bd6fef2
PineyPiney <86482622+PineyPiney@users.noreply.github.com>
2021-06-27 21:51:13 +0100

    Update PLP.java

--- src/main/java/com/pineypiney/plp/PLP.java
+++ src/main/java/com/pineypiney/plp/PLP.java
@@ -1,13 +1,11 @@
 package com.pineypiney.plp;
 
 import com.pineypiney.plp.registry.ModCommands;
-import com.pineypiney.plp.registry.ModSounds;
 import net.fabricmc.api.ModInitializer;
 
 public class PLP implements ModInitializer {
     @Override
     public void onInitialize() {
         ModCommands.registerCommands();
-        ModSounds.registerSounds();
     }
 }
